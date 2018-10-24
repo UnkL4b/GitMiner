baa7924b28043920c08710b5159dab37b94cdbaf
Danilo Vaz <danilovazb@gmail.com>
2018-10-24 02:38:11 -0300

    Update gitminer-v2.0.py

--- gitminer-v2.0.py
+++ gitminer-v2.0.py
@@ -27,11 +27,14 @@ class GitMiner(object):
         parser = argparse.ArgumentParser(self.descricao)
         parser.add_argument('-q','--query', metavar='{BLUE}\'filename:shadow path:etc\'{END}'.format(**colors), help='{YELLOW}Specify search term{END}'.format(**colors))
         parser.add_argument('-m','--module', metavar='{BLUE}wordpress{END}'.format(**colors), help='{YELLOW}Specify the search module{END}'.format(**colors), default=None)
+        parser.add_argument('-l','--list', help='{YELLOW}List modules{END}'.format(**colors), action='store_true')
         parser.add_argument('-o','--output', metavar='{BLUE}result.txt{END}'.format(**colors), help='{YELLOW}Specify the output file where it will be saved{END}'.format(**colors), default=None)
         parser.add_argument('-r', '--regex', metavar='{BLUE}\'/^\s*(.*?);?\s*$/gm\'{END}'.format(**colors), help='{YELLOW}Set regex to search in file{END}'.format(**colors), default=None)
         parser.add_argument('-c','--cookie', metavar='{BLUE}_octo=GH1.1.2098292984896.153133829439; _ga=GA1.2.36424941.153192375318; user_session=oZIxL2_ajeDplJSndfl37ddaLAEsR2l7myXiiI53STrfhqnaN; __Host-user_session_same_site=oXZxv9_ajeDplV0gAEsmyXiiI53STrfhDN; logged_in=yes; dotcom_user=unkl4b; tz=America%2FSao_Paulo; has_recent_activity=1; _gh_sess=MmxxOXBKQ1RId3NOVGpGcG54aEVnT1o0dGhxdGdzWVpySnFRd1dVYUk5TFZpZXFuTWxOdW1FK1IyM0pONjlzQWtZM2xtaFR3ZDdxlGMCsrWnBIdnhUN0tjVUtMYU1GeG5Pbm5DMThuWUFETnZjcllGOUNkRGUwNUtKOVJTaGR5eUJYamhWRE5XRnMWZZN3Y3dlpFNDZXL1NWUEN4c093RFhQd3RJQ1NBdmhrVDE3VVNiUFF3dHBycC9FeDZ3cFVXV0ZBdXZieUY5WDRlOE9ZSG5sNmRHUmllcmk0Up1MTcyTXZrN1RHYmJSdz09--434afdd652b37745f995ab55fc83{END}'.format(**colors), help='{YELLOW}Specify the cookie for your github{END}'.format(**colors), default=None)
         self.url = "https://github.com"
         self.args = parser.parse_args()
+        if self.args.list is True:
+            loadModule(self.args.module)
         if self.args.query is None or self.args.cookie is None:
             os.system('cls' if os.name == 'nt' else 'clear')
             parser.print_help()
